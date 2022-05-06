293a5c4a3c4cdd6f0be6ccd7d863bd9926dd1e4e
xlan11 <alan.kelly@gmail.com>
2022-05-06 15:02:12 +0100

    updated functions

--- index.js
+++ index.js
@@ -4,20 +4,21 @@
 //fetch('https://api.openweathermap.org/data/2.5/weather?q=Belfast&cnt=5&units=metric&appid=25419fc66df6465e6d2c90e8f096f2f5')
 
 //forecast
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&appid=25419fc66df6465e6d2c90e8f096f2f5')
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=55.12&lon=6.55&&units=metric&exclude=hourly,minutely&appid=25419fc66df6465e6d2c90e8f096f2f5')
   .then(response => response.json())
   .then(data => 
   {
         console.log(data)
         const {daily} = data
+        const weeklyWeather = daily
         let currentPop = document.getElementById("display-pop")
 
-        function temp(){
-            daily.forEach(pop => {
-              currentPop.innerHTML += `<li> ${data.daily[0].pop} </li>`
+        function precipitation(){
+            weeklyWeather.forEach(day => {
+              currentPop.innerHTML += `<li> ${day.pop * 100} %</li>`
             });
 
   }
-temp()
+precipitation()
 })
 
