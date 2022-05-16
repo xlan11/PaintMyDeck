a891419de68c2852ec33a900336949475ef0afb3
xlan11 <alan.kelly@gmail.com>
2022-05-16 12:18:18 +0100

    additional detail

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ fetch('https://api.openweathermap.org/data/2.5/onecall?lat=54.59&lon=5.93&units=
   {
 
         console.log(data)
-        const {daily, current, weather} = data
+        const {daily, current, weather, sunrise, sunset} = data
         // const icon = `https://openweathermap.org/img/wn/${daily.weather[0]["icon"]}@2x.png`
         let currentPop = document.getElementById("display-pop")
         const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
@@ -24,15 +24,27 @@ fetch('https://api.openweathermap.org/data/2.5/onecall?lat=54.59&lon=5.93&units=
             timestamp = day.dt
             test = new Date(timestamp  * 1000 );
             test.getDay();
+
+            sunriseTime = new Date(day.sunrise * 1000)
+              let sunriseHours = sunriseTime.getHours()
+              let sunriseMinutes = sunriseTime.getMinutes()
+              let sunriseTimeFormatted = "0" + sunriseHours + ":" + sunriseMinutes
+
+            sunsetTime = new Date(day.sunset * 1000)
+              let sunsetHours = sunsetTime.getHours()
+              let sunsetMinutes = sunsetTime.getMinutes()
+              let sunsetTimeFormatted = sunsetHours + ":" + sunsetMinutes
             
               currentPop.innerHTML +=
               `
               <div id="weather-card">
                 <li>
+                  <span id="weather-icon"><img src="https://openweathermap.org/img/wn/${daily[0].weather[0].icon}.png"/></span>
                   <span id="weather-day"><h2>${weekday[test.getDay()]}</h2></span>
                   <span id="weather-temp">Temp: ${(Math.floor(day.temp.day))}° (Min: ${(Math.floor(day.temp.min))}° - Max: ${(Math.floor(day.temp.max))})°<br/></span>  
-                  <span id="weather-pop">Precipitation chance: ${day.pop * 100}%</span>
-                  <!--<span id="weather-icon"><img src="https://openweathermap.org/img/wn/${daily[0].weather[0].icon}.png"/></span>-->
+                  <span id="weather-pop">Precipitation chance: ${(Math.floor(day.pop * 100))}%<br/></span>
+                  <span id="weather-sunrise">Sunrise: ${sunriseTimeFormatted} <br/></span>
+                  <span id="weather-sunset">Sunset: ${sunsetTimeFormatted}</span>
                 </li>
               </div>
               `
